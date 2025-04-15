7b04cca24686ce752bc15375ddfa86fe62028d79
muhammad.gad <muhammad.gad@ithraa.sa>
2025-04-15 10:37:59 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import ePub from 'epubjs';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'epub3';
+  private book: any;
+  private rendition: any;
+  private isBrowser: boolean;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+  ngAfterViewInit(): void {
+    if (this.isBrowser) {
+    this.book = ePub('/assets/moby-dick.epub'); // Make sure the path is correct
+
+    this.rendition = this.book.renderTo('viewer', {
+      width: '100%',
+      height: '100%',
+      allowScriptedContent: true // This tells EPUB.js to allow scripts in iframesallowScriptedContent: true // This tells EPUB.js to allow scripts in iframes
+    });
+
+    this.rendition.display();
+  }
+}
+
+  nextPage(){
+    this.rendition.next()
+  }
+  prevPage(){
+    this.rendition.prev();
+
+  }
+}
